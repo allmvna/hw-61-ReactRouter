9393f2577838d24a7169ac35e1594150cac574d3
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-10-21 03:46:58 +0600

    Added navigation and main application pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import Navigation from "./components/Navigation/Navigation.tsx";
+import {Route, Routes} from "react-router-dom";
+import Films from "./containers/Films/Films.tsx";
+import Books from "./containers/Books/Books.tsx";
+import Music from "./containers/Music/Music.tsx";
+import Portfolio from "./containers/Portfolio/Portfolio.tsx";
 
 const App = () => {
-  return <></>;
+  return (
+      <>
+          <Navigation />
+          <Routes>
+              <Route path="/" element={<Films/>} />
+              <Route path="/books" element={<Books/>} />
+              <Route path="/music" element={<Music/>} />
+              <Route path="/portfolio" element={<Portfolio/>} />
+          </Routes>
+      </>
+      )
 };
 
 export default App;
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-primary">
+                <div className="container">
+                    <NavLink to="/"><span className="navbar-brand mb-0 text-white fs-1">Мой дневник</span></NavLink>
+                    <div className="ms-auto">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/">♥ фильмы</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/books">♥ книги</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/music">♥ отрывки из песен</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/portfolio">портфолио</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Navigation;
